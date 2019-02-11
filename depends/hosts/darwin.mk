d917a3e5c1db11942c8787340f54bc1b3f6ca33b
Asher Dawes <asher.dawes@gmail.com>
2019-02-11 13:34:56 -0800

    Building with O0 optimizations

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -8,7 +8,7 @@ darwin_CXX=g++-6 -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysro
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
 
-darwin_release_CFLAGS=-O3
+darwin_release_CFLAGS=-O0
 darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
 
 darwin_debug_CFLAGS=-O0
